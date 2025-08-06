0ecec63061a7d52cfa0e52cee3af6c5a4dee1771
raquelkkkk <raquel.cassiano.rodrigues@escola.pr.gov.br>
2025-08-06 12:39:53 -0300

    06 de agosto

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-pergunta");
 const caixaAlternativas = document.querySelector (".caixa-alternativas");
-const caixaResultado =  document.querySelector (".texto-resultado");
+const textoResultado =  document.querySelector (".texto-resultado");
 
 const perguntas = [
     {
@@ -78,9 +78,38 @@ const perguntas = [
   
   let atual = 0;
   let perguntaAtual;
+  let historiaFinal = "";
   function mostraPergunta (){
-    perguntaAtual= perguntas[ atual];
+    if(atual >= perguntas.length){
+      mostraResultado();
+      return;
+    }
+    perguntaAtual= perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
   }
 
+  function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alernativas){
+      const botaoAlternativa = document.createElement("button");
+      botaoAlternativa.textContent = alternativa.texto;
+      botaoAlternativa.addEventlistener("click", () => respostaSelecionada
+      (alternativa));
+      caixaAlternativas.appendChild(botaoAlternativa);
+    }
+  }
+
+   function respostaSelecionada (opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + "";
+    atual++;
+    mostraPergunta();
+
+   }
+  
+function mostraResultado(){
+  caixaPerguntas.textContent = "Em 2049...";
+  textoResultado
+}
   mostraPergunta()
